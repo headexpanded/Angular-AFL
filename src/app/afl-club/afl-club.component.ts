87e60bb3368ec01344ad0374f198e02e38bdd0c6
moncon <markh@bellemoncon>
2023-07-01 11:39:07 +0200

    new  afl-club component

---
+++ src/app/afl-club/afl-club.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-afl-club',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      afl-club works!
+    </p>
+  `,
+  styleUrls: ['./afl-club.component.css']
+})
+export class AflClubComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AflClubComponent } from '../afl-club/afl-club.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AflClubComponent],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by club" />
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section class="results">
+      <app-afl-club></app-afl-club>
     </section>
   `,
   styleUrls: ['./home.component.css'],
