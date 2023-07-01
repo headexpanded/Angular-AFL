cafe251a48c7178cba071e72f5112e8aa91780c7
moncon <markh@bellemoncon>
2023-07-01 13:27:24 +0200

    new club service, club interface

---
+++ src/app/club.ts
@@ -0,0 +1,17 @@
+export interface Club {
+  pts: number;
+  played: number;
+  id: number;
+  for: number;
+  against: number;
+  rank: number;
+  goals_for: number;
+  losses: number;
+  draws: number;
+  behinds_for: number;
+  goals_against: number;
+  behinds_against: number;
+  percentage: number;
+  wins: number;
+  name: string;
+}
