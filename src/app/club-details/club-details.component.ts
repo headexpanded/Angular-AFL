eab5d4fcf6e69ce56514fde0aeddabbb4639b9e4
moncon <markh@bellemoncon>
2023-07-01 14:08:28 +0200

    club details component working

--- src/app/club-details/club-details.component.ts
+++ src/app/club-details/club-details.component.ts
@@ -8,13 +8,22 @@ import { Club } from '../club';
   selector: 'app-club-details',
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>club-details works! {{ clubId }}</p> `,
+  template: `
+    <article>
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ club?.name }}</h2>
+      </section>
+    </article>
+  `,
   styleUrls: ['./club-details.component.css'],
 })
 export class ClubDetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
+  clubService = inject(ClubService);
+  club: Club | undefined;
   clubId = -1;
   constructor() {
-    this.clubId = Number(this.route.snapshot.params['id']);
+    const clubId = Number(this.route.snapshot.params['id']);
+    this.club = this.clubService.getClubById(clubId);
   }
 }
