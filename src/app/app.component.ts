7dc7223e3a0f1016beb99ec8807fa1eeee4d9b07
moncon <markh@bellemoncon>
2023-07-01 13:42:24 +0200

    add Router Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
     <main>
-      <header class="brand-name">
-        <h1>Angular-AFL</h1>
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <h1>Angular-AFL</h1>
+        </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
