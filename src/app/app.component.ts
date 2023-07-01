421f85b7e0cb44d3dfe619d2da7d5f4f5ed5ada4
moncon <markh@bellemoncon>
2023-07-01 11:32:49 +0200

    Initial CSS for app & home components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,16 @@ import { HomeComponent } from './home/home.component';
   selector: 'app-root',
   standalone: true,
   imports: [HomeComponent],
-  template: `<main>
-    <header></header>
-    <section>
-      <app-home></app-home>
-    </section>
-  </main> `,
+  template: `
+    <main>
+      <header class="brand-name">
+        <h1>Angular-AFL</h1>
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
