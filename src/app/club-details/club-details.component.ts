8719bbc3b6f3ac432dcdecac1439a41e208824b3
moncon <markh@bellemoncon>
2023-07-01 13:44:34 +0200

    new club-details component

---
+++ src/app/club-details/club-details.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-club-details',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      club-details works!
+    </p>
+  `,
+  styleUrls: ['./club-details.component.css']
+})
+export class ClubDetailsComponent {
+
+}
