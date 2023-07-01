11eb50d2d5fc1500e0e2cb4c4afae7aab1dbee2d
moncon <markh@bellemoncon>
2023-07-01 13:30:54 +0200

    afl-ladder component

---
+++ src/app/afl-ladder/afl-ladder.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { Club } from '../club';
+
+@Component({
+  selector: 'app-afl-ladder',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <section class="listing">
+      
+    </section>
+  `,
+  styleUrls: ['./afl-ladder.component.css'],
+})
+export class AflLadderComponent {
+}
