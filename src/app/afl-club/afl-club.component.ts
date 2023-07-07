80714a0845d94706b956af29cc3422a6ea1ddf2c
moncon <markh@bellemoncon>
2023-07-07 12:38:06 +0200

    create afl-club.component.html

--- src/app/afl-club/afl-club.component.ts
+++ src/app/afl-club/afl-club.component.ts
@@ -8,57 +8,7 @@ import { Club } from '../club';
   selector: 'app-afl-club',
   standalone: true,
   imports: [CommonModule, RouterLink, RouterOutlet],
-  template: `
-    <div class="club-wrapper">
-      <div class="club-ladder-data">
-      
-          <a [routerLink]="['/details', club.id]">
-            <div class="club-rank">
-              <span>{{ club.rank }}</span>
-            </div>
-          </a>
-          <a [routerLink]="['/details', club.id]">
-            <div class="club-logo"><img [src]="club.logo" alt="Logo" /></div>
-          </a>
-          <a [routerLink]="['/details', club.id]">
-            <div class="club-name">
-              <span translate="no">{{ club.name }}</span>
-            </div>
-          </a>
-          <a [routerLink]="['/details', club.id]">
-            <div class="club-points">
-              <span>{{ club.pts }}</span>
-            </div>
-          </a>
-          <a [routerLink]="['/details', club.id]">
-            <div class="club-played">
-              <span>{{ club.played }}</span>
-            </div>
-          </a>
-          <a [routerLink]="['/details', club.id]">
-            <div class="club-wins">
-              <span>{{ club.wins }}</span>
-            </div>
-          </a>
-          <a [routerLink]="['/details', club.id]">
-            <div class="club-draws">
-              <span>{{ club.draws }}</span>
-            </div>
-          </a>
-          <a [routerLink]="['/details', club.id]">
-            <div class="club-losses">
-              <span>{{ club.losses }}</span>
-            </div>
-          </a>
-          <a [routerLink]="['/details', club.id]">
-            <div class="club-percentage">
-              <span>{{ club.percentage | number : '1.2-2' }}</span>
-            </div>
-          </a>
-      
-      </div>
-    </div>
-  `,
+  templateUrl: 'afl-club.component.html',
   styleUrls: ['./afl-club.component.css'],
 })
 export class AflClubComponent {
