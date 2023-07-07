9cd896c157653a4da9bae794c2772819d5f53df8
moncon <markh@bellemoncon>
2023-07-07 16:38:02 +0200

    header component for ladder

---
+++ src/app/ladder-header/ladder-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ladder-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: 'ladder-header.component.html',
+  styleUrls: ['./ladder-header.component.css']
+})
+export class LadderHeaderComponent {
+
+}
