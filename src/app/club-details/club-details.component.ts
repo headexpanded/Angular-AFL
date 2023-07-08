22aaf1649c2d2caad1496c3ac99baef4b984709f
moncon <markh@bellemoncon>
2023-07-08 14:54:20 +0200

    component for individual club details

--- src/app/club-details/club-details.component.ts
+++ src/app/club-details/club-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 
@@ -9,25 +9,19 @@ import { ClubService } from '../club.service';
   selector: 'app-club-details',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div class="content">
-      <h2>{{ club?.name }}</h2>
-    <p>{{ club?.played }}</p>
-    <p>{{ club?.wins }}</p>
-    <p>{{ club?.draws }}</p>
-    <p>{{ club?.losses }}</p>
-    </div>
-`,
+  templateUrl: './club-details.component.html',
   styleUrls: ['./club-details.component.css'],
 })
-
-export class ClubDetailsComponent {
-  route: ActivatedRoute = inject(ActivatedRoute);
-  clubService = inject(ClubService);
+export class ClubDetailsComponent implements OnInit {
   club: Club | undefined;
   clubId = -1;
-  
-  constructor() {
+
+  constructor(
+    private route: ActivatedRoute,
+    private clubService: ClubService
+  ) {}
+
+  ngOnInit(): void {
     this.clubId = parseInt(this.route.snapshot.params['id'], 10);
     this.clubService.getClubById(this.clubId).subscribe((club) => {
       this.club = club;
