96ea815670fb3e7cf63968435216f32eba6fa93d
moncon <markh@bellemoncon>
2023-07-08 14:54:52 +0200

    club.service efficiencies

--- src/app/club.service.ts
+++ src/app/club.service.ts
@@ -10,44 +10,46 @@ import { LogoService } from './logo.service';
   providedIn: 'root',
 })
 export class ClubService {
-
   // testing json data source: run json-server --watch
   url = 'http://localhost:3000/standings';
 
   // live json data source
   private standingsUrl = 'https://api.squiggle.com.au/?q=standings';
-  private teamUrl = 'https://api.squiggle.com.au/?q=teams;team=';
-  private giants = 'GWS Giants';
 
   constructor(private http: HttpClient, private logoService: LogoService) {}
 
+  private fetchStandings(): Observable<any> {
+    return this.http.get(this.standingsUrl, {
+      observe: 'body',
+      responseType: 'json',
+    });
+  }
+
   getAllClubs(): Observable<Club[]> {
-    return this.http
-      .get<Club[]>(this.standingsUrl, {
-        observe: 'body',
-        responseType: 'json',
+    return this.fetchStandings().pipe(
+      map((data: any) => {
+        const teams = this.logoService.addLogoToArray(data.standings);
+        teams.forEach((club: Club) => {
+          if (club.id === 9) {
+            club.name = 'GWS Giants';
+          }
+        });
+        return teams;
       })
-      .pipe(
-        map((data: any) => {
-          const teams = this.logoService.addLogoToArray(data.standings);
-          teams.forEach((club: Club) => {
-            if (club.id === 9) {
-              club.name = 'GWS Giants';
-            }
-          })
-          return teams;
-        })
-      );
+    );
   }
 
   getClubById(id: number): Observable<Club | undefined> {
-    const teamUrl = this.teamUrl + id;
-    console.log(teamUrl)
-    return this.http.get<Club>(teamUrl, {
-      observe: 'body',
-      responseType: 'json',
-    })
-      .pipe(map((data: any) => data.teams));
+    return this.fetchStandings().pipe(
+      map((data: any) => {
+        const teams = this.logoService.addLogoToArray(data.standings);
+        const club = teams.find((club: Club) => club.id === id);
+
+        if (club && club.id === 9) {
+          club.name = 'GWS Giants';
+        }
+        return club;
+      })
+    );
   }
 }
-
