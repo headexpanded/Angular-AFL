913c313aff9a925ae9b0a3f562e1c0bac750e3da
moncon <markh@bellemoncon>
2023-07-16 22:08:13 +0200

    add meta tags using Meta

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 import { Club } from '../club';
 import { ClubService } from '../club.service';
@@ -10,17 +11,32 @@ import { AflClubComponent } from '../afl-club/afl-club.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, LadderHeaderComponent, AflClubComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterOutlet,
+    LadderHeaderComponent,
+    AflClubComponent,
+  ],
   templateUrl: 'home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   clubLadder: Club[] = [];
   clubService: ClubService = inject(ClubService);
+  meta: Meta = inject(Meta);
 
   constructor() {
     this.clubService.getAllClubs().subscribe((clubLadder: Club[]) => {
       this.clubLadder = clubLadder;
     });
+    this.meta.addTags([
+      { name: 'description', content: 'An Ad-Free AFL Ladder' },
+      {
+        name: 'keywords',
+        content:
+          'AFL, AFL ladder, AFL ladder no ads, ad-free AFL ladder, ad free AFL, ad free AFL ladder, AFL ladder with no ads , AFL ladder without ads',
+      },
+    ]);
   }
 }
