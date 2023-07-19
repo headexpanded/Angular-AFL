0e7212c9b3e619b1f692085b1265ee395c2fb6bb
moncon <markh@bellemoncon>
2023-07-19 16:21:01 +0200

    env for prod, test

---
+++ src/environments/env.prod.ts
@@ -0,0 +1,7 @@
+
+
+export const env = {
+  // live json data source
+  standingsUrl: 'https://api.squiggle.com.au/?q=standings',
+  clubDataUrl: 'https://api.squiggle.com.au/?q=games;year=2023',
+};
