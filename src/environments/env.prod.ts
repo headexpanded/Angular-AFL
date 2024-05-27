6a43b83b72a1d683f1313ec47d93f9a00dd12c21
moncon <108305820+headexpanded@users.noreply.github.com>
2024-05-27 08:58:41 +0200

    Update env.prod.ts

--- src/environments/env.prod.ts
+++ src/environments/env.prod.ts
@@ -3,5 +3,5 @@
 export const env = {
   // live json data source
   standingsUrl: 'https://api.squiggle.com.au/?q=standings',
-  clubDataUrl: 'https://api.squiggle.com.au/?q=games;year=2023',
+  clubDataUrl: 'https://api.squiggle.com.au/?q=games;year=2024',
 };
