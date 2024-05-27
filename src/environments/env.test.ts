804f1d0618e037dab17bfbf8d2796c1e1c60830a
moncon <108305820+headexpanded@users.noreply.github.com>
2024-05-27 08:58:55 +0200

    Update env.test.ts

--- src/environments/env.test.ts
+++ src/environments/env.test.ts
@@ -2,7 +2,7 @@
 export const env = {
   // live json data source
   standingsUrl: 'https://api.squiggle.com.au/?q=standings',
-  clubDataUrl: 'https://api.squiggle.com.au/?q=games;year=2023',
+  clubDataUrl: 'https://api.squiggle.com.au/?q=games;year=2024',
   // testing json data source: run json-server --watch
   testUrl: 'http://localhost:3000/standings',
 };
